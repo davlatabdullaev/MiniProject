33d2e7abd09bb1038721fb8311e023cf6b16e739
davlatabdullaev <davlatdavlat0206@gmail.com>
2024-02-20 23:30:13 +0500

    updated

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+
+	"honnef.co/go/tools/config"
+)
+
+func main() {
+	cfg := config.Load()
+
+	log := logger.New(cfg.ServiceName)
+
+	newRedis := redis.New(cfg)
+
+	pgStore, err := postgres.New(context.Background(), cfg, log, newRedis)
+	if err != nil {
+		log.Error("error while connecting to db", logger.Error(err))
+		return
+	}
+	defer pgStore.Close()
+
+	services := service.New(pgStore, log, newRedis)
+
+	server := api.New(services, log)
+
+	log.Info("Service is running on", logger.Int("port", 8080))
+	if err = server.Run("localhost:8080"); err != nil {
+		panic(err)
+	}
+}
---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/cast"
+)
+
+type Config struct {
+	PostgresHost     string
+	PostgresPort     string
+	PostgresUser     string
+	PostgresPassword string
+	PostgresDB       string
+}
+
+func Load() Config {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("error!!!", err)
+	}
+
+	cfg := Config{}
+
+	cfg.PostgresHost = cast.ToString(getOrReturnDefault("POSTGRES_HOST", "localhost"))
+	cfg.PostgresPort = cast.ToString(getOrReturnDefault("POSTGRES_PORT", "5432"))
+	cfg.PostgresUser = cast.ToString(getOrReturnDefault("POSTGRES_USER", "user"))
+	cfg.PostgresPassword = cast.ToString(getOrReturnDefault("POSTGRES_PASSWORD", "password"))
+	cfg.PostgresDB = cast.ToString(getOrReturnDefault("POSTGRES_DB", "password"))
+
+
+	// cfg.ServiceName = cast.ToString(getOrReturnDefault("SERVICE_NAME", "store"))
+	// cfg.LoggerLevel = cast.ToString(getOrReturnDefault("LOGGER_LEVEL", "debug"))
+
+
+
+	return cfg
+}
+
+func getOrReturnDefault(key string, defaultValue interface{}) interface{} {
+	value := os.Getenv(key)
+	if value != "" {
+		return value
+	}
+
+	return defaultValue
+}
